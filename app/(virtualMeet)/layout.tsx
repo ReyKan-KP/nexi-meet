94271f284e840e1224ac1cf0208cef2238e9037f
Kanishaka Pranjal <kanishakpranjal@gmail.com>
2024-06-17 14:39:41 +0530

    Update layout.tsx

--- app/(virtualMeet)/layout.tsx
+++ app/(virtualMeet)/layout.tsx
@@ -4,18 +4,10 @@ import { ReactNode } from "react";
 import { Stream } from "stream";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "react-datepicker/dist/react-datepicker.css";
-const VirtualMeetLayout = ({
-  children,
-  session,
-}: {
-  children: ReactNode;
-  session: any;
-}) => {
+const VirtualMeetLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <main className="bg-dark-2">
-      <Provider session={session}>
-        <StreamVideoProvider>{children}</StreamVideoProvider>
-      </Provider>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
     </main>
   );
 };
